978df0719ac6838d99280005bd9b539fa2fd6b94
rileyrichter <bparker@gmail.com>
2020-06-06 18:06:01 -0500

    Adding starting files from private repository

---
+++ scripts/scripts.js
@@ -0,0 +1,77 @@
+// Pass co-workers name to success state for add co-workers page
+
+$('#add_coworkers').click(function() {
+    var cwaddfirst = $('#coworker1first').val();
+    var cwaddlast = $('#coworker1last').val();
+    $('#coworkname').html(cwaddfirst);
+    $('#coworknamelast').html(cwaddlast);
+});
+
+// Reload the page on click to add another co-worker
+$('#addcwagain').click(function() {
+    location.reload();
+});
+
+
+// Script for password matching and validation on the regiatration pages
+
+// check if password fields match on user input
+// for both the password & repeat-password fields
+// call the debounce function to run the input function after 500 milliseconds
+$('#password, #password-confirm').on('input', debounce(() => {
+	// if the value on the password field
+  // equals the value of the repeat-password field
+	if($('#password').val() === $('#password-confirm').val()){
+  	// click the .success field to initiate webflow interaction
+    // that shows a successful match
+		$('.confirm-click').click();
+  }
+  else{ // if the values don't match
+  	// click the .fail field to initiate webflow interaction
+    // that shows a failed match
+  	$('.not-confirmed-click').click();
+  }
+}, 500));
+
+
+// on submit button click
+$('#submit').click(()=>{
+	// if the passwords match
+	if($('#password').val() === $('#password-confirm').val()){
+  		// submit form
+		return true;
+  }
+  else{ // if the passwords don't match
+  	// prevent form submit
+  	return false;
+  }
+});
+
+// debounce function
+// the function calls a provided callback function that then runs after a set time period
+// the reason we use it here is to ensure the validate code runs after every 500 millisec
+// compared to running on every character the user enters as it would have without the debounce func
+function debounce(callback, wait) {
+	let timeout;
+  	return (...args) => {
+  		const context = this;
+    	clearTimeout(timeout);
+    	timeout = setTimeout(() => callback.apply(context, args), wait);
+  	};
+}
+
+//From the style page for the copy colors buttons
+ new Clipboard('.style-color-button');
+
+
+//Script to generate a password in Zapier
+function randomStr(len, arr) { 
+  var ans = ''; 
+  for (var i = len; i > 0; i--) { 
+    ans += 
+    arr[Math.floor(Math.random() * arr.length)]; 
+  } 
+  return ans; 
+} 
+
+output = [{password: randomStr(8, '123456789abcdefghijklmnopqrstuvwxyz')}];
